e6a32592deb80a31cbafa26c5b799cc034b17efb
Gagego Maciel <gagego.maciel@gmail.com>
2025-09-05 01:37:01 -0300

    Config Dockerfile

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-import { server } from './app.ts';
+import { server } from './app.ts'
 
-server.listen({ port: 3333 }).then(() => {
-  console.log('HTTP server running!');
-});
+server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+  console.log('HTTP server running!')
+})
